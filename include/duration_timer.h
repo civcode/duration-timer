2df2217579da0cfbd0651bc8891c0c15bfb28406
chris <ivi3001@yahoo.de>
2022-12-23 12:51:35 +0100

    initial commit

---
+++ include/duration_timer.h
@@ -0,0 +1,25 @@
+#ifndef DURATION_TIMER_H
+#define DURATION_TIMER_H
+
+#include <chrono>
+
+class DurationTimer {
+
+public:
+    DurationTimer(const std::chrono::microseconds interval);
+    DurationTimer(const std::chrono::milliseconds interval);
+    DurationTimer(const std::chrono::seconds interval);
+
+    bool is_expired();
+    void set_interval(const std::chrono::microseconds interval);
+    void set_interval(const std::chrono::milliseconds interval);
+    void set_interval(const std::chrono::seconds interval);
+
+private:
+    std::chrono::microseconds interval_;
+    std::chrono::high_resolution_clock::time_point target_;
+
+};
+
+
+#endif // DURATION_TIMER_H
